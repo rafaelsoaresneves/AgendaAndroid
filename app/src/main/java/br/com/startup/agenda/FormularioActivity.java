9033e8df1cca00e93320a652ca2843e2eb1c5e80
rafaelsoaresneves <rafael.soares.neves@gmail.com>
2016-10-26 00:24:31 -0200

    Criando Intents

--- app/src/main/java/br/com/startup/agenda/FormularioActivity.java
+++ app/src/main/java/br/com/startup/agenda/FormularioActivity.java
@@ -1,7 +1,11 @@
 package br.com.startup.agenda;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FormularioActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class FormularioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+
+        Button btSalvar = (Button) findViewById(R.id.form_btSalvar);
+
+        btSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(FormularioActivity.this, "Salvando", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/br/com/startup/agenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/startup/agenda/ListaAlunosActivity.java
@@ -1,8 +1,11 @@
 package br.com.startup.agenda;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class ListaAlunosActivity extends AppCompatActivity {
@@ -12,11 +15,21 @@ public class ListaAlunosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
 
-        String[] alunos = {"Rafael", "Felipe", "Sandra"};
+        String[] alunos = {"Rafael", "Felipe", "Sandra","Rafael", "Felipe", "Sandra","Rafael", "Felipe", "Sandra","Rafael", "Felipe", "Sandra"};
         ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
 
         ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
         listaAlunos.setAdapter(adapter);
+
+        Button botaoNovo = (Button) findViewById(R.id.bt_novo);
+
+        botaoNovo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentToForm = new Intent(ListaAlunosActivity.this, FormularioActivity.class);
+                startActivity(intentToForm);
+            }
+        });
 
     }
 }
